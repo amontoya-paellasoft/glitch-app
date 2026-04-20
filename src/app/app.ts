9657cd81ba383d99e373853f2cabfd0d53a8ce83
PBC5 <160127358+PBC5@users.noreply.github.com>
2026-04-20 11:25:44 +0200

    Estilos y Correcciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,8 @@ import { SimulationService } from './services/simulation-service';
 import { AgentPanel } from './components/agent-panel/agent-panel';
 import { ControlPanel } from './components/structure/control-panel/control-panel';
 import { Stats } from './components/stats/stats';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,9 +18,19 @@ import { RouterOutlet } from '@angular/router';
 export class App implements OnInit {
   public ws: WorkspaceService = inject(WorkspaceService);
   private simulationServ: SimulationService = inject(SimulationService);
+  private router = inject(Router);
+  
+  isMapRoute = true;
 
   ngOnInit() {
     this.simulationServ.iniciarSimulacion();
+    
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // La ruta del mapa es ''
+      this.isMapRoute = event.url === '/' || event.url === '';
+    });
   }
 
   get windowWidth(): number {
