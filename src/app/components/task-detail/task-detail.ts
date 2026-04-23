c6ca6beb477a2169b745f67622203ec922c52059
Agus MT <amontoya.paellasoft@gmail.com>
2026-04-23 11:50:34 +0200

    Detalle tarea creado

--- src/app/components/task-detail/task-detail.ts
+++ src/app/components/task-detail/task-detail.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, Input, OnChanges, output, signal } from '@angular/core';
 import { TaskDetailDTO } from '../../models/altorium/task-detail-dto';
 import { MOCK_TASK_DETAIL } from '../../mock/mock-task-detail';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-detail',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './task-detail.html',
   styleUrl: './task-detail.css',
 })
-export class TaskDetail implements OnInit {
+export class TaskDetail implements OnChanges {
+  @Input({ required: true }) taskId!: number;
+  cerrar = output<void>();
   taskData = signal<TaskDetailDTO | null>(null);
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     // TODO: reemplazar por llamada al servicio cuando esté el endpoint
     this.taskData.set(MOCK_TASK_DETAIL);
   }
